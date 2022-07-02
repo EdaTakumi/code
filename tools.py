b4f1ebb44075c2513aa975b583fe83a4b898a69d
Takumi Eda <B2278508@eagle.sophia.ac.jp>
2022-07-03 01:15:07 +0900

    add my_vis

--- tools.py
+++ tools.py
@@ -1,6 +1,11 @@
 import cv2 as cv
+import matplotlib
+import numpy as np
 import glob
+import matplotlib.pyplot as plt
 
+
+# 静止画から動画像を作成
 def MakeMovie(size, input_path, output_path, fps=5):
     pic_data = glob.glob(input_path)
     fourcc = cv.VideoWriter_fourcc('m','p','4','v')
@@ -11,19 +16,28 @@ def MakeMovie(size, input_path, output_path, fps=5):
     for i in range(len(pic_data)):
         img = pic_data[i]
         img = cv.imread(img)
-        img=cv.resize(img, size)
+        img = cv.resize(img, size)
         save.write(img)
     
     print('Completed!')
 
     save.release()
 
+# floファイルの可視化（カラー）
+def my_vis(flow):
+    mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv = np.zeros((flow.shape[0], flow.shape[1], 3), dtype = np.uint8)
+    hsv[..., 0] = ang / (2 * np.pi) * 180
+    hsv[..., 1] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+    hsv[..., 2] = 255
+    my_flow_color = cv.cvtColor(hsv, cv.COLOR_HSV2RGB)
+    my_flow_color = cv.cvtColor(my_flow_color, cv.COLOR_BGR2RGB)
+    return my_flow_color
+
 
 if __name__ == '__main__':
-    img = cv.imread("C:\\dataset\\scene-flow\\MPI-Sintel\\MPI-Sintel-complete\\training\\final\\market_2\\frame_0001.png")
-    height, width = img.shape[:2]
-    size = (width, height)
-    input_path = "C:\\dataset\\scene-flow\\MPI-Sintel\\MPI-Sintel-complete\\training\\final\\market_2\\*.png"
-    output_path = "C:\\Users\\takumi_eda\\Desktop\\code\\market_2.mp4"
-    MakeMovie(size, input_path, output_path)
-
+    flow = cv.readOpticalFlow("C:\\dataset\\scene-flow\\MPI-Sintel\\MPI-Sintel-complete\\training\\flow\\alley_1\\frame_0001.flo")
+    print(flow)
+    my_flow_color = my_vis(flow)
+    cv.imshow('flo', my_flow_color)
+    cv.waitKey(0)
